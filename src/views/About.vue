380ad9e8b5800c088e678264b15e97b0ae2be8ee
Mike Hartington <mikehartington@gmail.com>
2019-03-29 12:18:08 -0400

    updates

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,55 +1,68 @@
 <template>
-  <div class="ion-page">
-    <ion-header>
-      <ion-toolbar color="primary">
-        <ion-buttons slot="start">
-          <ion-menu-button></ion-menu-button>
-        </ion-buttons>
-        <ion-title>About</ion-title>
-        <ion-buttons slot="end">
-          <ion-button @click="presentPopover($event)">
-            <ion-icon slot="icon-only" name="more"></ion-icon>
-          </ion-button>
-        </ion-buttons>
-      </ion-toolbar>
-    </ion-header>
+<div class="ion-page">
+  <ion-header>
+    <ion-toolbar color="primary">
+      <ion-buttons slot="start">
+        <ion-menu-button></ion-menu-button>
+      </ion-buttons>
+      <ion-title>About</ion-title>
+      <ion-buttons slot="end">
+        <ion-button @click="presentPopover($event)">
+          <ion-icon slot="icon-only" name="more"></ion-icon>
+        </ion-button>
+      </ion-buttons>
+    </ion-toolbar>
+  </ion-header>
 
-    <ion-content>
-      <div class="about-header">
-        <img src="/assets/img/ionic-logo-white.svg" alt="ionic logo">
-      </div>
-      <div padding class="about-info">
-        <h4>Ionic Conference</h4>
+  <ion-content>
+    <div class="about-header">
+      <img src="/assets/img/ionic-logo-white.svg" alt="ionic logo">
+    </div>
+    <div padding class="about-info">
+      <h4>Ionic Conference</h4>
 
-        <ion-list lines="none">
-          <ion-item>
-            <ion-icon name="calendar" slot="start"></ion-icon>
-            <ion-label>Date</ion-label>
-            <ion-datetime displayFormat="MMM DD, YYYY" max="2056" :value="conferenceStart"></ion-datetime>
-          </ion-item>
+      <ion-list lines="none">
+        <ion-item>
+          <ion-icon name="calendar" slot="start"></ion-icon>
+          <ion-label>Date</ion-label>
+          <ion-datetime displayFormat="MMM DD, YYYY" max="2056" :value="conferenceStart"></ion-datetime>
+        </ion-item>
 
-          <ion-item>
-            <ion-icon name="pin" slot="start"></ion-icon>
-            <ion-label>Location</ion-label>
-            <ion-select>
-              <ion-select-option value="madison" selected>Madison, WI</ion-select-option>
-              <ion-select-option value="austin">Austin, TX</ion-select-option>
-              <ion-select-option value="chicago">Chicago, IL</ion-select-option>
-              <ion-select-option value="seattle">Seattle, WA</ion-select-option>
-            </ion-select>
-          </ion-item>
-        </ion-list>
+        <ion-item>
+          <ion-icon name="pin" slot="start"></ion-icon>
+          <ion-label>Location</ion-label>
+          <ion-select>
+            <ion-select-option value="madison" selected>Madison, WI</ion-select-option>
+            <ion-select-option value="austin">Austin, TX</ion-select-option>
+            <ion-select-option value="chicago">Chicago, IL</ion-select-option>
+            <ion-select-option value="seattle">Seattle, WA</ion-select-option>
+          </ion-select>
+        </ion-item>
+      </ion-list>
 
-        <p>
-          The Ionic Conference is a one-day conference featuring talks from the Ionic team. It is focused on Ionic applications being
-          built with Ionic 2. This includes migrating apps from Ionic 1 to Ionic 2, Angular concepts, Webpack, Sass, and many
-          other technologies used in Ionic 2. Tickets are completely sold out, and we’re expecting more than 1000 developers
-          – making this the largest Ionic conference ever!
-        </p>
-      </div>
-    </ion-content>
-  </div>
+      <p>
+        The Ionic Conference is a one-day conference featuring talks from the Ionic team. It is focused on Ionic applications being built with Ionic 2. This includes migrating apps from Ionic 1 to Ionic 2, Angular concepts, Webpack, Sass, and many other technologies
+        used in Ionic 2. Tickets are completely sold out, and we’re expecting more than 1000 developers – making this the largest Ionic conference ever!
+      </p>
+    </div>
+  </ion-content>
+</div>
 </template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import { mapGetters } from 'vuex';
+
+  @Component({
+    computed: mapGetters(['conferenceStart'])
+  })
+  export default class About extends Vue {
+
+    presentPopover(event){
+      console.log('click', event)
+    }
+  }
+</script>
 
 <style scoped>
 .about-header {
@@ -90,14 +103,3 @@
   margin-right: 10px;
 }
 </style>
-
-<script lang="ts">
-  import { Component, Prop, Vue } from 'vue-property-decorator';
-  import { mapGetters } from 'vuex';
-
-  @Component({
-    computed: mapGetters(['conferenceStart'])
-  })
-  export default class About extends Vue {
-  }
-</script>
